622b0c26cba5a7abf137c53bf76f5d7288153a0e
Mohamed <mhassankd35@gmail.com>
2023-03-25 01:48:57 -0500

    creates saved text state for both textboxes and passes down as props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
+import { useState } from "react";
+import TextBox from "./components/TextBox";
+import Upload from "./components/Upload";
+
 function App() {
-  return <div className="App"></div>;
+  const [savedText1, setSavedText1] = useState(
+    "This is a text box and the first line is programmed to hold exactly 20 words, after the limit is\n \nreached, the next paragraph will begin. The text box is limited to three paragraphs each containing 20, 25, and 10 words respectively. After the word\n\nlimit of 55 total words, user must remove words."
+  );
+  const [savedText2, setSavedText2] = useState(
+    "This is a text box and the first line is programmed to hold exactly 20 words, after the limit is\n \nreached, the next paragraph will begin. The text box is limited to three paragraphs each containing 20, 25, and 10 words respectively. After the word\n\nlimit of 55 total words, user must remove words."
+  );
+  console.log(savedText1, savedText2);
+  return (
+    <div
+      className="App"
+      style={{
+        display: "flex",
+
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <Upload />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          rowGap: "3rem",
+        }}
+      >
+        <TextBox
+          storageKey="storedText1"
+          setSavedText={setSavedText1}
+          savedText={savedText1}
+        />
+        <TextBox
+          storageKey="storedText2"
+          setSavedText={setSavedText2}
+          savedText={savedText2}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
